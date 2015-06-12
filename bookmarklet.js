04b22a8c491c23a8d8ad89a359b616d722bde2a7
gregormey <gregor@meyenberg.de>
2015-06-12 11:42:38 +0200

    add value badget

--- bookmarklet.js
+++ bookmarklet.js
@@ -861,7 +861,7 @@ body {
     height: 1.6rem;
     width: 1.6rem;
     border-radius: 50% !important;
-    background-color: ORGANGE !important;
+    background-color: #FE9A2E !important;
     line-height: 1.4rem;
     font-size: 0.9rem;
     font-weight: bold;
